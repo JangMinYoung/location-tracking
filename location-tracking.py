f5caa37fabbbde3b96240603f94366dc32eef5e3
JangMinYoung <min8540055@hanmail.net>
2018-12-06 07:25:09 +0900

    최종 수정

--- location-tracking.py
+++ location-tracking.py
@@ -36,7 +36,7 @@ def cross_correlation(y1, y2):
     # ax.set_xlabel('lag of y1 relative to y2')
 
     maxlag = lags[np.argmax(ccor)]
-    print("max correlation is at lag %d" % maxlag)
+    # print("max correlation is at lag %d" % maxlag)
     result = maxlag / 44100
 
     # plt.show()
@@ -46,40 +46,60 @@ def cross_correlation(y1, y2):
 
 
 def cal_Altitude(a, td10, td21, td02):
+    mul = 0
+    Altitude_angle = 0
     pow = math.pow(td10, 2) + math.pow(td21, 2) + math.pow(td02, 2)
     pow_sqr = math.sqrt(pow)
     constant = (math.sqrt(2) * 340) / (3 * a)
     # radian=math.radians(constant * pow_sqr)
     mul = constant * pow_sqr
-    print("radian:" + str(mul))
-    Altitude_angle = math.acos(mul)
-    print("Altitude_angle: " + str(Altitude_angle))
+    # print("radian:" + str(mul))
+    print("mul", str(mul))
+    if mul > 1:
+        return -2
+    else:
+        Altitude_angle = math.acos(mul)
+        return Altitude_angle
 
-    # Altitude_angle2=math.radians(86)
-    # print("Altitude_angle2: " + str(Altitude_angle2))
 
-    return Altitude_angle
+def cal_Bearing(a, td10, td02, td21, Altitude_angle):
+    excess_exist = 0
+    constant = (math.sqrt(3) * a) * math.cos(Altitude_angle)
 
+    cal_td10 = (340 * td10)
 
-def cal_Bearing(a, td21, Altitude_angle):
-    theta = 0
-    constant = (math.sqrt(3) * a) / 340
-    b = 8.166603115929353e-05
-    # pi_degree = math.degrees(math.pi)
-    cos_altitude = math.cos(Altitude_angle)
+    temp_td10 = (cal_td10 / constant)
+    if temp_td10 > 1 or temp_td10 < -1.1:
+        print("temp_td10",str(temp_td10))
+        excess_exist = 1
+    else:
+        bearing_td10 = math.acos(temp_td10)
+        bearing_td10 = bearing_td10 - (math.pi * (1 / 6))
 
-    x = ((td21 / constant) / cos_altitude)
-    # print("x: "+str(x))
-    arcoss_value = math.acos(x)
+    cal_td02 = (340 * td02)
+    temp_td02 = (cal_td02 / constant)
 
-    theta_1 = arcoss_value + (math.pi / 2)
-    # theta_2=(math.pi/2)-arcoss_value
-    # print(math.degrees(theta_2))
+    if temp_td02 > 1 or temp_td02 <= -1.1:
+        print("temp_td02", str(temp_td02))
+        excess_exist = 1
+    else:
+        bearing_td02 = math.acos(math.radians(temp_td02))
+        bearing_td02 = bearing_td02 - (math.pi * (5 / 6))
 
-    if math.degrees(theta_1) >= 90:
-        theta = 90 - math.degrees(arcoss_value)
-    print(theta)
-    return math.radians(theta)
+    cal_td21 = (340 * td21)
+    temp_td21 = (cal_td21 / constant)
+    if temp_td21 > 1 or temp_td21 < -1.1:
+        print("temp_td21", str(temp_td21))
+        excess_exist = 1
+    else:
+        bearing_td21 = math.acos(temp_td21)
+        bearing_td21 = bearing_td21 + (math.pi * (1 / 2))
+
+    if excess_exist == 0:
+        mean_bearing = (math.degrees(bearing_td10) + math.degrees(bearing_td02) + math.degrees(bearing_td21)) / 3
+        return math.radians(mean_bearing)
+    else:
+        return -2
 
 
 def location(r, Altitude_angle, Bearing_angle):
@@ -101,9 +121,6 @@ def graph(x_list, y_list, z_list, a):
 
     ax.scatter(x_list, y_list, z_list, c='b', marker='o', edgecolors='face')  # 위에서 정의한 x,y,z 가지고 그래프그린거다.
 
-    for i in range(len(z_list)):
-        print(z_list[i])
-
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
@@ -113,15 +130,14 @@ def graph(x_list, y_list, z_list, a):
 
 
 def wavefileTransfer(file_count):
-    print("=============wavefile->FFT=============")
+    print("=========================" + str(file_count) + "=========================")
     for i in range(1, 4):
-        fs, data = wavfile.read('./Experiment/' + str(file_count) + '_' + str(i) + '.wav')
-        print('샘플링 수 : ', fs)
+        fs, data = wavfile.read('./Experiment_5/' + str(file_count) + '_' + str(i) + '.wav')
+        # print('샘플링 수 : ', fs)
 
-        text = open('./Experiment/' + str(file_count) + '_' + str(i) + '.txt', 'w')
+        text = open('./Experiment_5/' + str(file_count) + '_' + str(i) + '.txt', 'w')
         for m in range(len(data)):
             text.write(str(data[m]) + "\n")
-        print(len(data))
         text.close()
 
 
@@ -129,18 +145,17 @@ def main():
     x_list = []
     y_list = []
     z_list = []
-    a = 2
+    a = 0.04
 
-    for i in range(1, 4):
-
+    for i in range(1, 11):
         file_count = i
         wavefileTransfer(file_count)
 
-        text1 = open('./Experiment/' + str(file_count) + '_1.txt', 'r')
-        text2 = open('./Experiment/' + str(file_count) + '_2.txt', 'r')
-        text3 = open('./Experiment/' + str(file_count) + '_3.txt', 'r')
+        text1 = open('./Experiment_5/' + str(file_count) + '_1.txt', 'r')
+        text2 = open('./Experiment_5/' + str(file_count) + '_2.txt', 'r')
+        text3 = open('./Experiment_5/' + str(file_count) + '_3.txt', 'r')
 
-        r = 0
+        r = 0.7
 
         y0 = []
         y1 = []
@@ -157,21 +172,23 @@ def main():
         td02 = cross_correlation(y0, y2)
 
         Altitude_angle = cal_Altitude(a, td10, td21, td02)
-        Bearing_angle = cal_Bearing(a, td21, Altitude_angle)
+        if Altitude_angle == -2:
+            print("고도각 초과")
+            continue
+        else:
+            Bearing_angle = cal_Bearing(a, td10, td02, td21, Altitude_angle)
+            if Bearing_angle == -2:
+                print("방위각 초과")
+                continue
+            else:
+                print('고도각', str(math.degrees(Altitude_angle)))
+                print('방위각', str(math.degrees(Bearing_angle)))
+                x, y, z = location(r, Altitude_angle, Bearing_angle)
+                x_list.append(x)
+                y_list.append(y)
+                z_list.append(z)
 
-        if i == 1:
-            r = 127
-        elif i == 2:
-            r = 254
-        elif i == 3:
-            r = 105
-
-        x, y, z = location(r, Altitude_angle, Bearing_angle)
-        x_list.append(x)
-        y_list.append(y)
-        z_list.append(z)
-
-        print("x:" + str(x), "y:" + str(y), "z:" + str(z))
+                print("x:" + str(x), "y:" + str(y), "z:" + str(z))
     graph(x_list, y_list, z_list, a)
 
 
